503f792553e9ae208d621ef28ce15a0d8ad38beb
Joe Byrne <jbyr279@aucklanduni.ac.nz>
2022-05-15 15:32:45 +1200

    Var Name Change

--- rotateAxis.m
+++ rotateAxis.m
@@ -1,8 +1,8 @@
-function [traj3Drot] = rotateAxis(traj3D, theta, axis)
-    if (axis == "overhead")
+function [traj3Drot] = rotateAxis(traj3D, theta, vp)
+    if (vp == "overhead")
         A = [1 0 0; 0 cos(theta) -sin(theta); 0 sin(theta) cos(theta)];
         traj3Drot = A*traj3D; 
-    elseif (axis == "profile")
+    elseif (vp == "profile")
         A = [cos(theta) sin(theta) 0; -sin(theta) cos(theta) 0; 0 0 1];
         traj3Drot = A*traj3D;
     end 
