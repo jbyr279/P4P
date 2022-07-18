6744334308a2f4419210b340e582a1ee72aeddf9
ache396 <Mountbatten1234>
2022-07-18 15:59:10 +1200

    Refactored experiment to randomise eccentricity too

--- dataParser.m
+++ dataParser.m
@@ -1,12 +1,18 @@
-function standardArray = dataParser(randomisedTrials, theta_v, degradation)
+function standardCell = dataParser(randomisedTrials, theta_v, degradation, eccentricity)
     % Convert to 4x4 standard
     standardArray = zeros([length(theta_v), length(degradation)]); % zeros(4)
+    standardCell = cell(1, length(eccentricity));
+
+    for i = 1:length(standardCell)
+        standardCell{i} = standardArray;
+    end
+
     for i = 1:length(randomisedTrials)
         dot_index = randomisedTrials{i}.degradation/min(degradation);
         theta_index = randomisedTrials{i}.theta_v/30 - 2;
-        standardArray(theta_index, dot_index) = standardArray(theta_index, dot_index) + randomisedTrials{i}.correct;
+        eccent_index = randomisedTrials{i}.eccentricity/20 + 1;
+        standardCell{eccent_index}(theta_index, dot_index) = standardCell{eccent_index}(theta_index, dot_index) + randomisedTrials{i}.correct;
     end
-%     standardArray = standardArray/numTrials;
 end
     
     
--- randomiseTrials.m
+++ randomiseTrials.m
@@ -1,16 +1,19 @@
-function trial_rand_1D = randomiseTrials(theta_v, degradation)
+function trial_rand_1D = randomiseTrials(theta_v, degradation, eccentricity)
     current_trials = cell(size(degradation,2), size(theta_v,2));
 
-    for i = 1:size(degradation,2)
-        for j = 1:size(theta_v,2)
-            current_trials{i, j}.degradation = degradation(i);
-            current_trials{i, j}.theta_v = theta_v(j);
-            current_trials{i, j}.correct = false;
-            current_trials{i, j}.inputKey = "";
+    for i = 1:length(degradation)
+        for j = 1:length(theta_v)
+            for k = 1:length(eccentricity)
+                current_trials{i, j, k}.degradation = degradation(i);
+                current_trials{i, j, k}.theta_v = theta_v(j);
+                current_trials{i, j, k}.correct = false;
+                current_trials{i, j, k}.inputKey = "";
+                current_trials{i, j, k}.eccentricity = eccentricity(k);
+            end
         end
     end
     
-    len = size(degradation,2) * size(theta_v,2);
+    len = length(degradation) * length(theta_v) * length(eccentricity);
     current_trials = reshape(current_trials,[1,len]);
     
     trial_rand_1D = current_trials(randperm(length(current_trials)));
